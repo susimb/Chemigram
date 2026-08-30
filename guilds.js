8b02abcd4505899ddda056a3fd9782f52f727bd2
Susi Marin <randommuffin.0@gmail.com>
2026-08-29 23:52:23 -0500

    Create guilds.js

---
+++ guilds.js
@@ -0,0 +1,26 @@
+export const guilds = [
+    {
+        id: 1,
+        name: "The Arcane Circle",
+        motto: "Knowledge through alchemy",
+        type: "PUBLIC",
+        members: 8,
+        emblem: "🔮"
+    },
+    {
+        id: 2,
+        name: "Order of the Phoenix",
+        motto: "From ashes, power",
+        type: "PUBLIC",
+        members: 12,
+        emblem: "🔥"
+    },
+    {
+        id: 3,
+        name: "The Silver Cauldron",
+        motto: "Perfection through patience",
+        type: "PRIVATE",
+        members: 6,
+        emblem: "⚗️"
+    }
+];
