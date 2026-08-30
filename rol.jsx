390d8c9d30a6e6235e0392713f5d6af68212ca66
Susi Marin <randommuffin.0@gmail.com>
2026-08-30 00:46:32 -0500

    Delete rol.jsx

--- rol.jsx
+++
@@ -1,9 +0,0 @@
-function rol ({ children, type = "default" }) {
-    return (
-        <span className={`badge badge-${type}`}>
-            {children}
-        </span>
-    );
-}
-
-export default rol;
